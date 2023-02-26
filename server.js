82e5c98b842810c681b2a8da86c97f1de2a4db63
irtizaa <irtizahussain.edu098@gmail.com>
2023-02-26 22:49:43 +0500

    Added project

---
+++ server.js
@@ -0,0 +1,58 @@
+var express = require('express');
+var cors = require('cors');
+var app = express();
+var GeoJSON = require('geojson');
+
+app.use(cors());
+
+function getcustomerdata(req,res){
+     var sql = require("mssql");
+
+    // config for your database
+    var config = {
+        // user: 'sde',
+        // password: 'Sdgis12345mn',
+        // server: '172.30.30.234', 
+		
+			user: 'sde',
+        password: 'Sdgis12345mn',
+        server: '103.31.82.102',
+        database: 'MN_South_Region',
+        options: {
+        trustedConnection: true
+        },
+       port: 1433
+    };
+
+    let conn = new sql.ConnectionPool(config);
+    conn.connect().then(function(){
+        let req = new sql.Request(conn);
+											
+		//req.query("SELECT * FROM MN_Customers_evw").then(function(result){										
+        req.query("SELECT * FROM MN_CUSTOMERS_EVW Where Region = 'South' AND Circuit_Owner = 'MultiNet'").then(function(result){
+            //console.log(result.recordset);
+            //res.send(JSON.stringify({recordset.recordsets}));
+            let data = result.recordset
+            //res.send(data)
+            res.send(GeoJSON.parse(data, { Point: ['Latitude', 'Longitude'] }))	
+            conn.close()
+        })
+        .catch(function(err){
+            console.log(err);
+            conn.close()
+        })
+    })
+    .catch(function(err){
+        console.log(err)
+    })
+}
+
+
+
+
+
+app.get('/customer', getcustomerdata)
+
+var server = app.listen(9001, function () {
+    console.log('Server is running on port 9001..');
+});
